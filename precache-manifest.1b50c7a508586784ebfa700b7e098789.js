b61f53fe50640d591138f2fdf975850a10c10b35
Raja's <gkrajareddy@gmail.com>
2020-10-21 20:33:42 +0530

    Updates

---
+++ precache-manifest.1b50c7a508586784ebfa700b7e098789.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28c061ae5759bfdd70f84fbe3d38936",
+    "url": "/react-portfolio-unschool/index.html"
+  },
+  {
+    "revision": "f9ae039c69ef79eb26aa",
+    "url": "/react-portfolio-unschool/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "945c8437f2d37edc8b25",
+    "url": "/react-portfolio-unschool/static/css/main.156f604f.chunk.css"
+  },
+  {
+    "revision": "f9ae039c69ef79eb26aa",
+    "url": "/react-portfolio-unschool/static/js/2.8d28bb5c.chunk.js"
+  },
+  {
+    "revision": "7271fad03e259c1944e82c2b05258fc2",
+    "url": "/react-portfolio-unschool/static/js/2.8d28bb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945c8437f2d37edc8b25",
+    "url": "/react-portfolio-unschool/static/js/main.5099f86a.chunk.js"
+  },
+  {
+    "revision": "6e1036444ace4c083133",
+    "url": "/react-portfolio-unschool/static/js/runtime-main.18f84a3a.js"
+  },
+  {
+    "revision": "4a6ca9de90e13d92eeaf77099ef05f28",
+    "url": "/react-portfolio-unschool/static/media/bootstrap4.4a6ca9de.png"
+  },
+  {
+    "revision": "436cff5f03f38c1c0e472b3fb167a0b3",
+    "url": "/react-portfolio-unschool/static/media/css3.436cff5f.png"
+  },
+  {
+    "revision": "0726a0c396d004c818db95c138a3192e",
+    "url": "/react-portfolio-unschool/static/media/flask.0726a0c3.png"
+  },
+  {
+    "revision": "c3d882ebcc009714d36909ea88579b2d",
+    "url": "/react-portfolio-unschool/static/media/html5.c3d882eb.png"
+  },
+  {
+    "revision": "82c50a10fb44dca388fdfa63cec49fb5",
+    "url": "/react-portfolio-unschool/static/media/javascript.82c50a10.png"
+  },
+  {
+    "revision": "ffd01a3c1ecec5c0a33e203b75b53175",
+    "url": "/react-portfolio-unschool/static/media/myphoto.ffd01a3c.png"
+  },
+  {
+    "revision": "be2cace33f1da8314d1bfec4cc1660d4",
+    "url": "/react-portfolio-unschool/static/media/mysql.be2cace3.png"
+  },
+  {
+    "revision": "2f4718597b404390490d0ca3487cba4f",
+    "url": "/react-portfolio-unschool/static/media/project2.2f471859.jpg"
+  },
+  {
+    "revision": "2054ba0428442d71425661bf4f29c95b",
+    "url": "/react-portfolio-unschool/static/media/project3.2054ba04.jpg"
+  },
+  {
+    "revision": "aa172609998ea31780bc9090761e6448",
+    "url": "/react-portfolio-unschool/static/media/python.aa172609.png"
+  },
+  {
+    "revision": "69d69642e98012f45f7cf312050b7dfa",
+    "url": "/react-portfolio-unschool/static/media/react.69d69642.png"
+  }
+]);
